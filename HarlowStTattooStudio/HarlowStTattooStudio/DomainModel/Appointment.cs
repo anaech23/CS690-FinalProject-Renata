78589f71c25902333b8b1e65190a465bf66a1808
anaech23 <anaech23@gmail.com>
2026-07-02 21:53:34 -0400

    feat: complete iteration 1 with full scheduling and tracking functionality

---
+++ HarlowStTattooStudio/HarlowStTattooStudio/DomainModel/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarlowStTattooStudio.DomainModel
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int ClientId { get; set; }
+        public int ArtistId { get; set; }
+        public DateTime AppointmentStart { get; set; }
+        public DateTime AppointmentEnd { get; set; }
+        public int? FollowupAppointmentId { get; set; }
+        public decimal DepositAmount { get; set; }
+        public bool DepositPaid { get; set; }
+        public AppointmentStatus Status { get; set; }
+    }
+}
